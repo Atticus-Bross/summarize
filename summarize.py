aec840e1374331292fa9831d3ae563cccb3b7799
Atticus Bross <atb5098@utulsa.edu>
2024-10-30 09:08:03 -0500

    reformatted the code

--- summarize.py
+++ summarize.py
@@ -13,44 +13,53 @@ Options:
   -f --function=<function> Specify the body of an inclusion function
 """
 
-from docopt import docopt
-import nltk
 import sys
 from collections import defaultdict
 from collections.abc import Callable
-from typing import TextIO
-from typing import Sequence
-from types import UnionType
 from math import log
+from types import UnionType
+from typing import Sequence
+from typing import TextIO
+
+import nltk
+from docopt import docopt
+
+
 def load_document(textfile: TextIO) -> list[str]:
     """Reads a text file and returns a list of sentences"""
     text = [line.strip() for line in textfile.readlines()]
     text = nltk.sent_tokenize(' '.join(text))
     return text
-def non_alnums(string:str)->str:
+
+
+def non_alnums(string: str) -> str:
     """non_alnums(string)
     Gives the characters in a string that are not alphanumeric
 
     string: the string"""
-    non_alnums2:str=''
+    non_alnums2: str = ''
     for character in string:
         if not character.isalnum() and character not in non_alnums2:
-            non_alnums2=non_alnums2+character
+            non_alnums2 = non_alnums2 + character
     return non_alnums2
-def many_split(string:str,chars:list)->list[str]:
+
+
+def many_split(string: str, chars: list) -> list[str]:
     """many_split(string, chars)
     Splits a string on many characters
 
     string: the string to be split
     chars: the characters to be split on (expressed as a list)"""
-    if len(chars)<1:
+    if len(chars) < 1:
         return [string]
-    chars2:list=chars.copy()
-    split=string.split(chars2.pop(0))
-    full_split:list=[]
+    chars2: list = chars.copy()
+    split = string.split(chars2.pop(0))
+    full_split: list = []
     for word in split:
-        full_split.extend(many_split(word,chars2))
+        full_split.extend(many_split(word, chars2))
     return full_split
+
+
 def deep_unpack(seq: Sequence[Sequence], ignores: type | UnionType = str) -> list:
     """deep_unpack(seq, ignores=str)
     Unpacks a sequence of sequences into a single sequence
@@ -68,46 +77,53 @@ def deep_unpack(seq: Sequence[Sequence], ignores: type | UnionType = str) -> lis
         else:
             unpacked.append(element)
     return unpacked
-def clean_word(word:str)->list[str]:
+
+
+def clean_word(word: str) -> list[str]:
     """clean_word(word)
     Cleans up a word, sometimes splitting it into multiple words
 
     word: the word to be cleaned up"""
-    to_remove:str=non_alnums(word)
-    cleaned:str=word.strip(to_remove)
-    return many_split(cleaned,list(to_remove))
-def remove_all(l:list,value)->list:
+    to_remove: str = non_alnums(word)
+    cleaned: str = word.strip(to_remove)
+    return many_split(cleaned, list(to_remove))
+
+
+def remove_all(l: list, value) -> list:
     """remove_all(l, value)
     Removes all instances of a value
 
     l: the list
     value: the value to remove"""
-    return_list:list=l.copy()
+    return_list: list = l.copy()
     for _ in range(return_list.count(value)):
         return_list.remove(value)
     return return_list
-def clean_sentence(sentence:list[str])->list[str]:
+
+
+def clean_sentence(sentence: list[str]) -> list[str]:
     """clean_sentence(sentence)
     Cleans up a sentence
 
     sentence: the sentence to clean up"""
-    cleaned:list=[clean_word(word) for word in sentence]
-    cleaned=deep_unpack(cleaned)
-    return remove_all(cleaned,'')
+    cleaned: list = [clean_word(word) for word in sentence]
+    cleaned = deep_unpack(cleaned)
+    return remove_all(cleaned, '')
+
+
 # [TODO] Remove non-word symbols from terms, maybe more?
 def clean_text(text: list[str]) -> list[list[str]]:
     """Transform text into a list of terms for each sentence"""
     sentences: list[list[str]] = []
     for line in text:
-        #word_tokenize returns words in all lowercase
+        # word_tokenize returns words in all lowercase
         sentence = [word.casefold()
                     for word in nltk.word_tokenize(line)]
-        sentence:list=clean_sentence(sentence)
+        sentence: list = clean_sentence(sentence)
         if len(sentence) > 0:
             sentences.append(sentence)
     return sentences
 
-                
 
 # [TODO] Implement Term Frequency calculation for document, term
 def calculate_tf(sentences: list[list[str]]) -> list[dict]:
@@ -117,9 +133,9 @@ def calculate_tf(sentences: list[list[str]]) -> list[dict]:
     matrix: list[dict] = []
     for index, sentence in enumerate(sentences):
         matrix.append({})
-        terms:set=set(sentence)
+        terms: set = set(sentence)
         for term in terms:
-            matrix[index][term]=sentence.count(term)/len(sentence)
+            matrix[index][term] = sentence.count(term) / len(sentence)
     return matrix
 
 
@@ -128,24 +144,27 @@ def calculate_idf(sentences: list[list[str]]) -> dict[str, float]:
     """Calculate the Inverse `Document'(Sentence) Frequency of each term.
     Returns a table of terms and their idf values."""
     matrix: dict[str, float] = defaultdict(float)
-    words:set=set(deep_unpack(sentences))
+    words: set = set(deep_unpack(sentences))
     for word in words:
-        #the number of Trues in is_in is equal to the number of sentences that contain word
-        is_in:list=list(map(lambda x:word in x,sentences))
-        matrix[word]=len(sentences)/is_in.count(True)
-        matrix[word]=log(matrix[word])
+        # the number of Trues in is_in is equal to the number of sentences that contain word
+        is_in: list = list(map(lambda x: word in x, sentences))
+        matrix[word] = len(sentences) / is_in.count(True)
+        matrix[word] = log(matrix[word])
     return matrix
 
-def calculate_tf_idf(tf:dict,idf_matrix:[str,float])->dict[str,float]:
+
+def calculate_tf_idf(tf: dict, idf_matrix: [str, float]) -> dict[str, float]:
     """calculate_tf_idf(tf, idf_matrix)
     Calculates the tf-idf of all the terms in tf
 
     tf: a dictionary that contains the term frequency of a series of a series of terms
     idf_matrix: a dictionary contains the inverse document frequency of a series of terms"""
-    tf_idf_matrix:dict={}
+    tf_idf_matrix: dict = {}
     for term in tf.keys():
-        tf_idf_matrix[term]=tf[term]*idf_matrix[term]
+        tf_idf_matrix[term] = tf[term] * idf_matrix[term]
     return tf_idf_matrix
+
+
 # [TODO] Implement sentence scoring
 def score_sentences(tf_matrix: list[dict], idf_matrix: dict[str, float], sentences: list[list[str]]) -> list[float]:
     """Score each sentence for importance based on the terms it contains.
@@ -153,10 +172,10 @@ def score_sentences(tf_matrix: list[dict], idf_matrix: dict[str, float], sentenc
     Returns a table whose keys are the indices of sentences of the text
     and values are the sum of tf-idf scores of each word in the sentence"""
     scores: list[float] = []
-    tf_idf_matrix:list=[calculate_tf_idf(sentence,idf_matrix) for sentence in tf_matrix]
+    tf_idf_matrix: list = [calculate_tf_idf(sentence, idf_matrix) for sentence in tf_matrix]
     for index, sentence in enumerate(sentences):
-        total_tf_idf:float=sum(tf_idf_matrix[index].values())
-        scores.append(total_tf_idf/len(sentence))
+        total_tf_idf: float = sum(tf_idf_matrix[index].values())
+        scores.append(total_tf_idf / len(sentence))
     return scores
 
 
@@ -168,7 +187,7 @@ def threshold_inclusion(text: list[str], scores: list[float], threshold=1):
         if score >= threshold * avg_score:
             summary += [text[index]]
     return summary
-    
+
 
 def summarize(text: list[str], inclusion: Callable) -> str:
     """Summarizes a given text using tf-idf and a given inclusion function."""
@@ -191,13 +210,13 @@ if __name__ == '__main__':
     # Threshold value may need adjustment. It might be appropriate to expand this
     # to allow inclusion function and inclusion criteria to be specified as
     # commandline options
-    #defining func here because pycharm does not understand why it will always be created in the if statements below
-    func = lambda:1
+    # defining func here because pycharm does not understand why it will always be created in the if statements below
+    func = lambda: 1
     if arguments['--function']:
-        lines:list=arguments['--function'].split(r'\n')
-        #add indentation so exec will work properly
-        lines=list(map(lambda x:' '+x,lines))
-        lines.insert(0,'def func(text, scores):')
+        lines: list = arguments['--function'].split(r'\n')
+        # add indentation so exec will work properly
+        lines = list(map(lambda x: ' ' + x, lines))
+        lines.insert(0, 'def func(text, scores):')
         exec('\n'.join(lines))
     else:
         func = lambda text, scores: threshold_inclusion(text, scores, threshold=1)
--- tests/test_summarize.py
+++ tests/test_summarize.py
@@ -1,31 +1,42 @@
-"""Module testing the Summarize Functions
+"""Test Summarize:
+
+Module testing the Summarize Functions
 
 Completed by Atticus Bross on 2024-10-29 for DS-1043"""
 
-from summarize import *
 from math import isclose
-from random import randint,choice
-def test_non_alnums()->None:
+from random import randint, choice
+
+from summarize import *
+
+
+def test_non_alnums() -> None:
     """test_non_alnums()
     Tests the non_alnums function"""
-    assert non_alnums('the rabbit')==' '
-    assert non_alnums('t[6}!ty')=='[}!'
-    assert non_alnums('|thy&')=='|&'
-    assert non_alnums('{{{***^&^&')=='{*^&'
-def test_many_split()->None:
+    assert non_alnums('the rabbit') == ' '
+    assert non_alnums('t[6}!ty') == '[}!'
+    assert non_alnums('|thy&') == '|&'
+    assert non_alnums('{{{***^&^&') == '{*^&'
+
+
+def test_many_split() -> None:
     """test_many_split()
     Tests the many_split function"""
-    assert many_split('abc|efg|hij',['|'])==['abc','efg','hij']
-    assert many_split('abc|efg!hij',['|','!'])==['abc','efg','hij']
-    assert many_split('ab?cd|ef%gh|ij?kl|mn%op',['%','|','?'])==['ab','cd','ef','gh','ij','kl','mn','op']
-    assert many_split('abcdef',['9','p','['])==['abcdef']
-    assert many_split('[}|:*&',[])==['[}|:*&']
-def test_clean_word()->None:
+    assert many_split('abc|efg|hij', ['|']) == ['abc', 'efg', 'hij']
+    assert many_split('abc|efg!hij', ['|', '!']) == ['abc', 'efg', 'hij']
+    assert many_split('ab?cd|ef%gh|ij?kl|mn%op', ['%', '|', '?']) == ['ab', 'cd', 'ef', 'gh', 'ij', 'kl', 'mn', 'op']
+    assert many_split('abcdef', ['9', 'p', '[']) == ['abcdef']
+    assert many_split('[}|:*&', []) == ['[}|:*&']
+
+
+def test_clean_word() -> None:
     """test_clean_word()
     Tests the clean_word function"""
-    assert clean_word('|abc|efg|hij|')==['abc','efg','hij']
-    assert clean_word('{}{}|}{}abc|efg!hij%^&*')==['abc','efg','hij']
-    assert clean_word('[}|:*&')==['']
+    assert clean_word('|abc|efg|hij|') == ['abc', 'efg', 'hij']
+    assert clean_word('{}{}|}{}abc|efg!hij%^&*') == ['abc', 'efg', 'hij']
+    assert clean_word('[}|:*&') == ['']
+
+
 def test_deep_unpack() -> None:
     """test_deep_unpack()
     Tests the deep_unpack function"""
@@ -33,73 +44,108 @@ def test_deep_unpack() -> None:
     assert deep_unpack([[['abc', None], [], 2.34], [[], [True, 3]]]) == ['abc', None, 2.34, True, 3]
     assert deep_unpack([[(1, 2, 3), ('abc', 'efg')], ['abc']], tuple) == [(1, 2, 3), ('abc', 'efg'), 'a', 'b', 'c']
     assert deep_unpack([[(1, 2, 3), ('abc', 'efg')], ['abc']], tuple | str) == [(1, 2, 3), ('abc', 'efg'), 'abc']
-def test_remove_all()->None:
+
+
+def test_remove_all() -> None:
     """test_remove_all()
     Tests the remove_all function"""
-    assert remove_all([1,2,3,4],4)==[1,2,3]
-    assert remove_all([1,2,3,3,3,4],5)==[1,2,3,3,3,4]
-    assert remove_all([1],1)==[]
-    assert remove_all([1,1,1,1],1)==[]
-    assert remove_all([1,2,3,3,3,4],3)==[1,2,4]
-def test_clean_sentence()->None:
+    assert remove_all([1, 2, 3, 4], 4) == [1, 2, 3]
+    assert remove_all([1, 2, 3, 3, 3, 4], 5) == [1, 2, 3, 3, 3, 4]
+    assert remove_all([1], 1) == []
+    assert remove_all([1, 1, 1, 1], 1) == []
+    assert remove_all([1, 2, 3, 3, 3, 4], 3) == [1, 2, 4]
+
+
+def test_clean_sentence() -> None:
     """test_clean_sentence()
     Tests the clean_sentence function"""
-    assert clean_sentence([word.casefold() for word in nltk.word_tokenize('They may be replaced by new “federated” platforms.')])==['they','may','be','replaced','by','new','federated','platforms']
-    assert clean_sentence([word.casefold() for word in nltk.word_tokenize('Jürgen Habermas, the German philosopher who coined the term _legitimization crisis_,')])==['jürgen','habermas','the','german','philosopher','who','coined','the','term','legitimization','crisis']
-    assert clean_sentence([word.casefold() for word in nltk.word_tokenize('etc. and/the mr.')])==['etc','and','the','mr']
-def test_clean_text()->None:
+    assert clean_sentence(
+        [word.casefold() for word in nltk.word_tokenize('They may be replaced by new “federated” platforms.')]) == [
+               'they', 'may', 'be', 'replaced', 'by', 'new', 'federated', 'platforms']
+    assert clean_sentence([word.casefold() for word in nltk.word_tokenize(
+        'Jürgen Habermas, the German philosopher who coined the term _legitimization crisis_,')]) == ['jürgen',
+                                                                                                      'habermas', 'the',
+                                                                                                      'german',
+                                                                                                      'philosopher',
+                                                                                                      'who', 'coined',
+                                                                                                      'the', 'term',
+                                                                                                      'legitimization',
+                                                                                                      'crisis']
+    assert clean_sentence([word.casefold() for word in nltk.word_tokenize('etc. and/the mr.')]) == ['etc', 'and', 'the',
+                                                                                                    'mr']
+
+
+def test_clean_text() -> None:
     """test_clean_text()
     Tests the clean_text function"""
-    assert clean_text(['They may be replaced by new “federated” platforms.'])==[['they','may','be','replaced','by','new','federated','platforms']]
-    assert clean_text(['Illustration by Edmon de Haro','Jürgen Habermas, the German philosopher who coined the term _legitimization crisis_,'])==[['illustration','by','edmon','de','haro'],['jürgen','habermas','the','german','philosopher','who','coined','the','term','legitimization','crisis']]
-    assert clean_text(['etc. and/the mr.','To/from/cube [qr] i/o?','(the#get&^where}'])==[['etc','and','the','mr'],['to','from','cube','qr','i','o'],['the','get','where']]
-def test_calculate_tf()->None:
+    assert clean_text(['They may be replaced by new “federated” platforms.']) == [
+        ['they', 'may', 'be', 'replaced', 'by', 'new', 'federated', 'platforms']]
+    assert clean_text(['Illustration by Edmon de Haro',
+                       'Jürgen Habermas, the German philosopher who coined the term _legitimization crisis_,']) == [
+               ['illustration', 'by', 'edmon', 'de', 'haro'],
+               ['jürgen', 'habermas', 'the', 'german', 'philosopher', 'who', 'coined', 'the', 'term', 'legitimization',
+                'crisis']]
+    assert clean_text(['etc. and/the mr.', 'To/from/cube [qr] i/o?', '(the#get&^where}']) == [
+        ['etc', 'and', 'the', 'mr'], ['to', 'from', 'cube', 'qr', 'i', 'o'], ['the', 'get', 'where']]
+
+
+def test_calculate_tf() -> None:
     """test_calculate_tf()
     Tests the calculate_tf function"""
-    with open(r'C:\Users\user\PycharmProjects\summarize\examples\social.md','r',encoding='utf-8') as mdfile:
-        test:list=load_document(mdfile)
-    test=clean_text(test)
-    test2:list=calculate_tf(test)
-    assert isclose(test2[0]['created'],1/194)
-    assert isclose(test2[10]['as'],3/66)
-    assert isclose(test2[20]['no'],3/64)
-    assert len(test2)==len(test)
-    index:int=randint(0,len(test)-1)
-    test_choice:list=test[index]
-    test2_choice:dict=test2[index]
+    with open(r'C:\Users\user\PycharmProjects\summarize\examples\social.md', 'r', encoding='utf-8') as mdfile:
+        test: list = load_document(mdfile)
+    test = clean_text(test)
+    test2: list = calculate_tf(test)
+    assert isclose(test2[0]['created'], 1 / 194)
+    assert isclose(test2[10]['as'], 3 / 66)
+    assert isclose(test2[20]['no'], 3 / 64)
+    assert len(test2) == len(test)
+    index: int = randint(0, len(test) - 1)
+    test_choice: list = test[index]
+    test2_choice: dict = test2[index]
     for word in test2_choice.keys():
         assert word in test_choice
-def test_calculate_idf()->None:
+
+
+def test_calculate_idf() -> None:
     """test_calculate_idf()
     Tests the calculate_idf function"""
-    with open(r'C:\Users\user\PycharmProjects\summarize\examples\social.md','r',encoding='utf-8') as mdfile:
-        test:list=load_document(mdfile)
-    test=clean_text(test)
-    test2:dict=calculate_idf(test)
-    assert isclose(test2['https'],log(112/30))
-    assert isclose(test2['social'],log(112/22))
-    assert isclose(test2['a'],log(112/51))
+    with open(r'C:\Users\user\PycharmProjects\summarize\examples\social.md', 'r', encoding='utf-8') as mdfile:
+        test: list = load_document(mdfile)
+    test = clean_text(test)
+    test2: dict = calculate_idf(test)
+    assert isclose(test2['https'], log(112 / 30))
+    assert isclose(test2['social'], log(112 / 22))
+    assert isclose(test2['a'], log(112 / 51))
     assert choice(list(test2.keys())) in deep_unpack(test)
-def test_calculate_tf_idf()->None:
+
+
+def test_calculate_tf_idf() -> None:
     """test_calculate_tf_idf()
     Tests the calculate_tf_idf function"""
-    assert calculate_tf_idf({'a':1.0},{'a':2.0})=={'a':2.0}
-    assert calculate_tf_idf({'back':3.0,'line':2.0,'test':4.0},{'test':2.0,'back':1.0,'line':3.0})=={'back':3.0,'line':6.0,'test':8.0}
-    assert calculate_tf_idf({'back': 3.0, 'line': 2.0, 'test': 4.0}, {'skd':1.2,'test': 2.0,'sda':5.4, 'back': 1.0, 'line': 3.0,'lkj':9.8}) == {
+    assert calculate_tf_idf({'a': 1.0}, {'a': 2.0}) == {'a': 2.0}
+    assert calculate_tf_idf({'back': 3.0, 'line': 2.0, 'test': 4.0}, {'test': 2.0, 'back': 1.0, 'line': 3.0}) == {
         'back': 3.0, 'line': 6.0, 'test': 8.0}
-def test_score_sentences()->None:
+    assert calculate_tf_idf({'back': 3.0, 'line': 2.0, 'test': 4.0},
+                            {'skd': 1.2, 'test': 2.0, 'sda': 5.4, 'back': 1.0, 'line': 3.0, 'lkj': 9.8}) == {
+               'back': 3.0, 'line': 6.0, 'test': 8.0}
+
+
+def test_score_sentences() -> None:
     """test_score_sentences()
     Tests the score_sentences function"""
-    with open(r'C:\Users\user\PycharmProjects\summarize\examples\stories.md','r',encoding='utf-8') as mdfile:
-        clean:list=load_document(mdfile)
-    clean=clean_text(clean)
-    tf:list=calculate_tf(clean)
-    idf:dict=calculate_idf(clean)
-    test:list=score_sentences(tf,idf,clean)
-    assert isclose(test[30],0.14230652336740154)
-    assert isclose(test[7],0.17909274372242093)
-    assert isclose(test[14],0.10086149660969546)
-    assert len(test)==len(clean)
+    with open(r'C:\Users\user\PycharmProjects\summarize\examples\stories.md', 'r', encoding='utf-8') as mdfile:
+        clean: list = load_document(mdfile)
+    clean = clean_text(clean)
+    tf: list = calculate_tf(clean)
+    idf: dict = calculate_idf(clean)
+    test: list = score_sentences(tf, idf, clean)
+    assert isclose(test[30], 0.14230652336740154)
+    assert isclose(test[7], 0.17909274372242093)
+    assert isclose(test[14], 0.10086149660969546)
+    assert len(test) == len(clean)
+
+
 test_non_alnums()
 test_many_split()
 test_clean_word()
